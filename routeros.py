f80cfdb9785a8014a6283a429b974131297b04e6
Tim van Werkhoven <tvwerkhoven@github>
2021-11-19 22:21:18 +0100

    Init, plugin collects cpu, mem, tx, rx

---
+++ routeros.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# routeros collectd plugin built on librouteros-py
+#
+# Use this in collectd.conf:
+# LoadPlugin python
+# <Plugin python>
+#     ModulePath "/path/to/routeros-collectd"
+#     Import "routeros"
+#     <Module routeros>
+#         Host "127.0.0.1"
+#         User "username"
+#         Password "password"
+#
+#     </Module>
+# </Plugin>
+
+import collectd
+import ssl
+from librouteros import connect
+
+
+HOST = '127.0.0.1'		# host to connect to
+USER = 'user'			# username, set in system -> users
+PASSWORD = 'password'	# username, set in system -> users
+INTERFACE = 'ether0'	# interface to track tx/rx for, usually used for WAN interface
+API = None				# used by script to track connection to router
+
+def config_func(config):
+	for node in config.children:
+		key = node.key.upper()
+		val = node.values[0]
+
+		if key == 'HOST':
+			global HOST
+			HOST = val
+			collectd.info('routeros plugin: Using overridden HOST %s' % HOST)
+		elif key == 'USER':
+			global USER
+			USER = val
+			collectd.info('routeros plugin: Using overridden USER %s' % USER)
+		elif key == 'PASSWORD':
+			global PASSWORD
+			PASSWORD = val
+			collectd.info('routeros plugin: Using overridden PASSWORD')
+		elif key == 'INTERFACE':
+			global INTERFACE
+			INTERFACE = val
+			collectd.info('routeros plugin: Using overridden INTERFACE %s' % INTERFACE)
+		else:
+			collectd.info('routeros plugin: Unknown config key "%s"' % key)
+
+def init_func():
+	collectd.info('routeros plugin: init config')
+	ctx = ssl.create_default_context()
+	ctx.check_hostname = False
+	ctx.set_ciphers('ADH:@SECLEVEL=0')
+	ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+	global API
+	API = connect(
+		username=USER,
+		password=PASSWORD,
+		host=HOST,
+		ssl_wrapper=ctx.wrap_socket,
+		port=8729
+	)
+
+def read_func():
+	# collectd.info('routeros plugin: read data')
+
+	# First create desired path.
+	interfaces = API.path('interface')
+	# {'.id': '*E', 'name': 'WAN_VLAN', 'type': 'vlan', 'mtu': 1500, 'actual-mtu': 1500, 'l2mtu': 1594, 'mac-address': 'DE:AD:BE:EF:70:1C', 'last-link-down-time': 'nov/19/2021 21:12:30', 'last-link-up-time': 'nov/19/2021 21:12:53', 'link-downs': 1, 'rx-byte': 141089032, 'tx-byte': 11222723, 'rx-packet': 117721, 'tx-packet': 80098, 'rx-drop': 0, 'tx-drop': 0, 'tx-queue-drop': 0, 'rx-error': 0, 'tx-error': 0, 'fp-rx-byte': 141089032, 'fp-tx-byte': 0, 'fp-rx-packet': 117721, 'fp-tx-packet': 0, 'running': True, 'disabled': False}
+
+	resources = API.path('system', 'resource')
+	# {'uptime': '4d7h27m6s', 'version': '6.47 (stable)', 'build-time': 'Jun/02/2020 07:38:00', 'free-memory': 48701440, 'total-memory': 134217728, 'cpu': 'MIPS 74Kc V4.12', 'cpu-count': 1, 'cpu-frequency': 600, 'cpu-load': 2, 'free-hdd-space': 108892160, 'total-hdd-space': 134217728, 'write-sect-since-reboot': 311063, 'write-sect-total': 550052, 'bad-blocks': '0.1', 'architecture-name': 'mipsbe', 'board-name': 'RB2011UiAS', 'platform': 'MikroTik'}
+
+	# Dispatch values to collectd
+	val = collectd.Values(host='rb2011', plugin='cpu', type='percent', type_instance='active')
+	val.plugin = 'routeros'
+	val.dispatch(values=[tuple(resources)[0]['cpu-load']])
+
+	val2 = collectd.Values(host='rb2011', plugin='memory', type='memory', type_instance='free')
+	val2.plugin = 'routeros'
+	val2.dispatch(values=[tuple(resources)[0]['free-memory']])
+
+	# Get INTERFACE Tx/Rx data
+	for intf in interfaces:
+		if (intf['name'] == INTERFACE):
+			val3 = collectd.Values(host='rb2011', plugin='interface', type='if_rx_octets', type_instance=INTERFACE)
+			val3.plugin = 'routeros'
+			val3.dispatch(values=[intf['rx-byte']])
+			val4 = collectd.Values(host='rb2011', plugin='interface', type='if_tx_octets', type_instance=INTERFACE)
+			val4.plugin = 'routeros'
+			val4.dispatch(values=[intf['tx-byte']])
+
+collectd.register_config(config_func)
+collectd.register_init(init_func)
+collectd.register_read(read_func)
